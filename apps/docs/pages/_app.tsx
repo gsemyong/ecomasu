0e6fc5cb02de0c91bcf2bbd4b67719ff75bdd289
gsemy <gsemyong@gmail.com>
2023-08-23 17:21:34 +0300

    Docs initial setup

---
+++ apps/docs/pages/_app.tsx
@@ -0,0 +1,19 @@
+import type { AppProps } from "next/app";
+
+import { Space_Grotesk } from "next/font/google";
+
+// If loading a variable font, you don't need to specify the font weight
+const spaceGrotesk = Space_Grotesk({ subsets: ["latin"] });
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${spaceGrotesk.style.fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  );
+}
---
+++ apps/docs/theme.config.tsx
@@ -0,0 +1,13 @@
+import { DocsThemeConfig } from "nextra-theme-docs";
+
+export default {
+  logo: <span>My Nextra Documentation</span>,
+  primaryHue: {
+    dark: 47.9,
+    light: 47.9,
+  },
+  project: {
+    link: "https://github.com/shuding/nextra",
+  },
+  // ... other theme options
+} satisfies DocsThemeConfig;
