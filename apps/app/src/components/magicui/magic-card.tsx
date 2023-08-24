9e3b9fd815d1938fa64dd0741b718fef0811721d
gsemy <gsemyong@gmail.com>
2023-08-24 08:31:44 +0300

    Fix build

--- apps/app/src/components/magicui/magic-card.tsx
+++ apps/app/src/components/magicui/magic-card.tsx
@@ -1,4 +1,6 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
 
