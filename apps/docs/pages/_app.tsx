a4b2b94a059c88b97b465d907987f2a9d64b92aa
gsemy <gsemyong@gmail.com>
2023-08-24 17:28:50 +0300

    Some adjustments to docs

--- apps/docs/pages/_app.tsx
+++ apps/docs/pages/_app.tsx
@@ -2,6 +2,8 @@ import type { AppProps } from "next/app";
 
 import { Space_Grotesk } from "next/font/google";
 
+import "./styles/globals.css";
+
 // If loading a variable font, you don't need to specify the font weight
 const spaceGrotesk = Space_Grotesk({ subsets: ["latin"] });
 
--- apps/docs/theme.config.tsx
+++ apps/docs/theme.config.tsx
@@ -1,13 +1,25 @@
 import { DocsThemeConfig } from "nextra-theme-docs";
+import { ShadowIcon } from "@radix-ui/react-icons";
 
 export default {
-  logo: <span>My Nextra Documentation</span>,
-  primaryHue: {
-    dark: 47.9,
-    light: 47.9,
+  logo: (
+    <ShadowIcon
+      style={{
+        color: "hsl(47.9,95.8%,53.1%)",
+        width: "2rem",
+        height: "2rem",
+      }}
+    />
+  ),
+  primaryHue: 47.9,
+  nextThemes: {
+    defaultTheme: "dark",
   },
   project: {
     link: "https://github.com/shuding/nextra",
+  },
+  footer: {
+    component: null,
   },
   // ... other theme options
 } satisfies DocsThemeConfig;
